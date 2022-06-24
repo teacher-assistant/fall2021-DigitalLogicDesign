0e4ab4a8812a7d9e320825e2f08339b7c490ae2a
Omid Reza <omid77.orh@gmail.com>
2022-06-24 05:50:37 +0430

    Create Website

---
+++ components/HomeworkStatusTable.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="alert alert-dark">
+    	<h3>
+    		Below table is updated <span class="alert-link">a few days after deadlines</span>
+    	</h3>
+    	<div class="table-responsive">
+		<table class="table table-primary table-striped">
+		  <thead>
+		    <tr>
+		      <th scope="col">#</th>
+		      <th scope="col">Student Number</th>
+		      <th scope="col">Quiz1</th>
+		      <th scope="col">Quiz2</th>
+		      <th scope="col">Quiz3</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    <tr v-for="(student, index) in students">
+		      <th scope="row">{{ index+1 }}</th>
+		      <td>{{ student.id }}</td> 
+		      <td> {{ student.Quiz1 }}</td>
+		      <td> {{ student.Quiz2 }}</td>
+		      <td> {{ student.Quiz3 }}</td>
+		    </tr>
+		  </tbody>
+		</table>
+    	</div>
+    </div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			students:[]
+		}
+	},
+  	mounted() {
+	    this.$nextTick(()=>{
+		  	this.$axios.$get('https://teacher-assistant.github.io/fall2021-DigitalLogicDesign/HomeWorks.json')
+	        .then(response=>this.students=response)
+	    })
+  	}
+}
+</script>
